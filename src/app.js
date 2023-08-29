4c2db018698b89017c3100bc6129ed7c2072f16a
joseRamirezgomez <joseramirezgomez00@gmail.com>
2023-08-29 11:16:42 -0500

    add gitignore, controllers y routes

--- src/app.js
+++ src/app.js
@@ -7,4 +7,12 @@ const app = express()
 app.use(express.json())
 
 app.use(indexRoutes)
-app.use('/api',employeesRoutes)
+app.use('/api',employessRoutes)
+
+app.use((req, res, next) =>{
+    res.status(404).json({
+        message: 'EndPoint No Encontrado'
+    })
+})
+
+export default app
