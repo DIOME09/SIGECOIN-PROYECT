a8480154a099405ced1e4553d7e300d38346dfc4
Diomedez <diomedesd732@gmail.com>
2023-09-04 09:06:18 -0500

    tabla,envio,metododepago

--- db/datos.sql
+++ db/datos.sql
@@ -1,1 +1,25 @@
-/*agregar aqui los datos que ingresen a la DB ponerle un comentario indicando de que tabla es*/
+//datos tabla envio
+
+INSERT INTO envio ( empresa)VALUES("zapatex");
+INSERT INTO envio ( empresa)VALUES("bodegon");
+INSERT INTO envio ( empresa)VALUES("calzadiana");
+INSERT INTO envio ( empresa)VALUES("zaptejil");
+INSERT INTO envio ( empresa)VALUES("adidastex");
+INSERT INTO envio ( empresa)VALUES("laquinta");
+INSERT INTO envio ( empresa)VALUES("expotex");
+INSERT INTO envio ( empresa)VALUES("juansport");
+INSERT INTO envio ( empresa)VALUES("danilotex");
+INSERT INTO envio ( empresa)VALUES("rematessport");
+
+//datos metododepago
+
+INSERT INTO metododepago (metodo_pago,condicion_pago)VALUES("paypal,nequi,mastercad,bancolombia","efectivo,transferencias,tarjetascredito/debito");
+INSERT INTO metododepago (metodo_pago,condicion_pago)VALUES("paypal,nequi,mastercad,bancolombia","efectivo,transferencias,tarjetascredito/debito");
+INSERT INTO metododepago (metodo_pago,condicion_pago)VALUES("paypal,nequi,mastercad,bancolombia","efectivo,transferencias,tarjetascredito/debito");
+INSERT INTO metododepago (metodo_pago,condicion_pago)VALUES("paypal,nequi,mastercad,bancolombia","efectivo,transferencias,tarjetascredito/debito");
+INSERT INTO metododepago (metodo_pago,condicion_pago)VALUES("paypal,nequi,mastercad,bancolombia","efectivo,transferencias,tarjetascredito/debito");
+INSERT INTO metododepago (metodo_pago,condicion_pago)VALUES("paypal,nequi,mastercad,bancolombia","efectivo,transferencias,tarjetascredito/debito");
+INSERT INTO metododepago (metodo_pago,condicion_pago)VALUES("paypal,nequi,mastercad,bancolombia","efectivo,transferencias,tarjetascredito/debito");
+INSERT INTO metododepago (metodo_pago,condicion_pago)VALUES("paypal,nequi,mastercad,bancolombia","efectivo,transferencias,tarjetascredito/debito");
+INSERT INTO metododepago (metodo_pago,condicion_pago)VALUES("paypal,nequi,mastercad,bancolombia","efectivo,transferencias,tarjetascredito/debito");
+INSERT INTO metododepago (metodo_pago,condicion_pago)VALUES("paypal,nequi,mastercad,bancolombia","efectivo,transferencias,tarjetascredito/debito");
