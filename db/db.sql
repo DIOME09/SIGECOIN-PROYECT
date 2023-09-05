0da700e0a0dd67420b33af77170c3d76c99a9d9e
Dannyg01 <dguzmanvargas78@gmail.com>
2023-09-05 09:24:54 -0500

    add Correcion de la factura

--- db/db.sql
+++ db/db.sql
@@ -23,3 +23,8 @@ INSERT INTO metododepago (metodo_pago,condicion_pago) VALUES ("tarjetacredito","
 INSERT INTO metododepago (metodo_pago,condicion_pago) VALUES ("pagocontraentrega","efectivo-transferencia");
 INSERT INTO metododepago (metodo_pago,condicion_pago) VALUES ("tarjetadebito","transferencia");
 INSERT INTO metododepago (metodo_pago,condicion_pago) VALUES ("transferencia","transferencia");
+
+//datos de factura
+INSERT INTO factura (id_cliente, id_producto, id_pago, id_envio, cantidad, precioUnitario, precioTotal) VALUES(2, 5, 1, 1, 9, 2000, 18000),
+(3, 6, 2, 2, 7, 4000, 28000),(4, 7, 8, 8, 10, 7000, 70000), (5, 3, 4, 4, 5, 3000, 15000), (1, 2, 3, 3, 6, 1000, 6000),
+(6, 1, 5, 5, 4, 5000, 20000),(7, 8, 7, 7, 8, 10000, 80000);
