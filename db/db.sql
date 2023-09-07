6f3715de8bd12824737fb06bfb7bfac6ff29942d
Dannyg01 <dguzmanvargas78@gmail.com>
2023-09-06 19:22:22 -0500

    add correccion db.sql

--- db/db.sql
+++ db/db.sql
@@ -21,4 +21,11 @@ INSERT INTO factura (id_usuario, id_productos, cantidad, precioUnitario, precioT
 (3, 6, 2, 2, 7, 4000, 28000),(4, 7, 8, 8, 10, 7000, 70000), (5, 3, 4, 4, 5, 3000, 15000), (1, 2, 3, 3, 6, 1000, 6000),
 (6, 1, 5, 5, 4, 5000, 20000),(7, 8, 7, 7, 8, 10000, 80000);
 
-/*datos prod*/
+/*datos usuarios*/
+
+INSERT INTO usuarios (tipodedocumento, num_documento, nombres, apellidos, direccion, email, contraseña, id_roles) VALUES 
+("cedula", 1077876678, "John", "Valderrama", "Calle 12# 18-40 Medellin", "john.val@example.com", 12345,),
+("cedula", 1077876679, "Julian", "Espinoza", "Calle 10# 11-20 Pitalito", "julian.espi@gmail.com", 12346,),
+("cedula", 1077876676, "Oscar", "Parra", "Calle 13# 12-30 Neiva", "oscar.pa@gmail.com", 12347,),
+("cedula", 1077876675, "Viviana", "Peres", "Calle 14# 13-50 Bogota", "vivi.pe@example.com", 12348,);
+    
